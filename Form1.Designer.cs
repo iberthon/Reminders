ae859248d7c9992232a09b6fb069c131e7587839
iberthon <ian@berthon.uk.com>
2017-06-02 17:19:22 +0100

    Added project files

---
+++ Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Reminders
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.remindersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.next7DaysToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.next14DaysToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.next21DaysToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.next28DaysToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.labelDate = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 273);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(563, 37);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(476, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Ok";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(0, 51);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(563, 222);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.remindersToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(563, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // remindersToolStripMenuItem
+            // 
+            this.remindersToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.next7DaysToolStripMenuItem,
+            this.next14DaysToolStripMenuItem,
+            this.next21DaysToolStripMenuItem,
+            this.next28DaysToolStripMenuItem});
+            this.remindersToolStripMenuItem.Name = "remindersToolStripMenuItem";
+            this.remindersToolStripMenuItem.Size = new System.Drawing.Size(75, 20);
+            this.remindersToolStripMenuItem.Text = "&Reminders";
+            // 
+            // next7DaysToolStripMenuItem
+            // 
+            this.next7DaysToolStripMenuItem.Name = "next7DaysToolStripMenuItem";
+            this.next7DaysToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+            this.next7DaysToolStripMenuItem.Text = "Next &7 Days";
+            this.next7DaysToolStripMenuItem.Click += new System.EventHandler(this.next7DaysToolStripMenuItem_Click);
+            // 
+            // next14DaysToolStripMenuItem
+            // 
+            this.next14DaysToolStripMenuItem.Checked = true;
+            this.next14DaysToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.next14DaysToolStripMenuItem.Name = "next14DaysToolStripMenuItem";
+            this.next14DaysToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+            this.next14DaysToolStripMenuItem.Text = "Next 1&4 Days";
+            this.next14DaysToolStripMenuItem.Click += new System.EventHandler(this.next14DaysToolStripMenuItem_Click);
+            // 
+            // next21DaysToolStripMenuItem
+            // 
+            this.next21DaysToolStripMenuItem.Name = "next21DaysToolStripMenuItem";
+            this.next21DaysToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+            this.next21DaysToolStripMenuItem.Text = "Next 2&1 Days";
+            this.next21DaysToolStripMenuItem.Click += new System.EventHandler(this.next21DaysToolStripMenuItem_Click);
+            // 
+            // next28DaysToolStripMenuItem
+            // 
+            this.next28DaysToolStripMenuItem.Name = "next28DaysToolStripMenuItem";
+            this.next28DaysToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+            this.next28DaysToolStripMenuItem.Text = "Next 2&8 Days";
+            this.next28DaysToolStripMenuItem.Click += new System.EventHandler(this.next28DaysToolStripMenuItem_Click);
+            // 
+            // labelDate
+            // 
+            this.labelDate.Dock = System.Windows.Forms.DockStyle.Top;
+            this.labelDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDate.Location = new System.Drawing.Point(0, 24);
+            this.labelDate.Name = "labelDate";
+            this.labelDate.Size = new System.Drawing.Size(563, 24);
+            this.labelDate.TabIndex = 3;
+            this.labelDate.Text = "The Date";
+            this.labelDate.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(563, 310);
+            this.Controls.Add(this.labelDate);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem remindersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem next7DaysToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem next14DaysToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem next21DaysToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem next28DaysToolStripMenuItem;
+        private System.Windows.Forms.Label labelDate;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.SQLite;
+
+namespace Reminders
+{
+    public partial class Form1 : Form
+    {
+        private int showFor;
+        private DateTime theDay;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            showFor = 14;
+            next14DaysToolStripMenuItem.Checked = true;
+
+            theDay = DateTime.Today;
+
+            String[] args = Environment.GetCommandLineArgs();
+            if (args.Length > 1)
+            {
+                Debug.Print("Args: {0}", String.Join(", ", args));
+                theDay = DateTime.Parse(args[1]);
+            }
+            labelDate.Text = String.Format("{0:MMMM dd, yyyy}", theDay);
+        }
+
+        public void OnThisDay()
+        {
+            SQLiteConnection db;
+
+            string dbfile = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\reminders.db3";
+            Debug.Print("DB File: {0}", dbfile);
+
+            db = new SQLiteConnection("Data Source=" + dbfile);
+            db.Open();
+            using (SQLiteCommand cmd = db.CreateCommand())
+            {
+                cmd.CommandText = String.Format("SELECT * FROM onthisday WHERE mth={0} AND dy={1}", theDay.Month, theDay.Day);
+                //Debug.Print("OnThisDay: {0}", cmd.CommandText);
+                using (SQLiteDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        richTextBox1.Clear();
+                        richTextBox1.BackColor = Color.White;
+                        richTextBox1.SelectionFont = new Font("Arial", 12, FontStyle.Bold | FontStyle.Underline);
+                        richTextBox1.SelectedText = "On This Day\n";
+
+                        while (reader.Read())
+                        {
+                            var y = reader.GetValue(reader.GetOrdinal("yr"));
+                            int cat = Convert.ToInt32(reader.GetValue(reader.GetOrdinal("cat")));
+                            String name = Convert.ToString(reader.GetValue(reader.GetOrdinal("name")));
+
+                            richTextBox1.SelectionFont = new Font("Arial", 12, FontStyle.Regular);
+                            if (Convert.IsDBNull(y))
+                            {
+                                richTextBox1.SelectionColor = Color.Tomato;
+                                richTextBox1.SelectedText = String.Format("{0}\n", name);
+                            }
+                            else
+                            {
+                                richTextBox1.SelectedText = String.Format("{0} ({1})\n", name, Convert.ToInt32(y));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public void Reminders()
+        {
+            SQLiteConnection db;
+
+            string dbfile = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\reminders.db3";
+
+            db = new SQLiteConnection("Data Source=" + dbfile);
+            db.Open();
+            using (SQLiteCommand cmd = db.CreateCommand())
+            {
+                cmd.CommandText = String.Format("SELECT dt, name, strftime('%Y','{0:yyyy-MM-dd}')||substr(dt,5) AS due, (julianday(strftime('%Y','{1:yyyy-MM-dd}')||substr(dt,5)) - julianday(Date('{2:yyyy-MM-dd}'))) AS diff, strftime('%Y','{3:yyyy-MM-dd}')||substr(dt,5)-dt as age FROM reminders WHERE diff<{4} AND diff>=0 order by due", theDay, theDay, theDay, theDay, showFor);
+                //Debug.Print("Reminders: {0}", cmd.CommandText);
+                using (SQLiteDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            DateTime dt = Convert.ToDateTime(reader.GetValue(reader.GetOrdinal("dt")));
+                            String name = Convert.ToString(reader.GetValue(reader.GetOrdinal("name")));
+                            DateTime due = Convert.ToDateTime(reader.GetValue(reader.GetOrdinal("due")));
+                            int diff = Convert.ToInt32(reader.GetValue(reader.GetOrdinal("diff")));
+                            int age = Convert.ToInt32(reader.GetValue(reader.GetOrdinal("age")));
+
+                            String yrs = (age != 1 ? "years" : "year");
+                            String dys = (diff != 1 ? "days" : "day");
+
+                            Color c = Color.Black;
+                            if (diff == 0)
+                            {
+                                c = Color.Green;
+                            }
+                            else if (diff <= 7)
+                            {
+                                c = Color.Red;
+                            }
+
+                            richTextBox1.SelectionColor = c;
+                            richTextBox1.SelectionFont = new Font("Arial", 12, FontStyle.Bold | FontStyle.Underline);
+                            richTextBox1.SelectedText = String.Format("\n{0:MMMM dd, yyyy}\n", due);
+
+                            richTextBox1.SelectionColor = c;
+                            richTextBox1.SelectionFont = new Font("Arial", 12, FontStyle.Regular);
+                            richTextBox1.SelectedText = String.Format("{0} ({1} {2}) in {3} {4}.\n", name, age, yrs, diff, dys);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //this.Text = String.Format("Reminders - {0:MMMM dd, yyyy}", theDay);
+
+            OnThisDay();
+            Reminders();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void next7DaysToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            showFor = 7;
+            next7DaysToolStripMenuItem.Checked = true;
+            next14DaysToolStripMenuItem.Checked = false;
+            next21DaysToolStripMenuItem.Checked = false;
+            next28DaysToolStripMenuItem.Checked = false;
+
+            Reminders();
+        }
+
+        private void next14DaysToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            showFor = 14;
+            next7DaysToolStripMenuItem.Checked = false;
+            next14DaysToolStripMenuItem.Checked = true;
+            next21DaysToolStripMenuItem.Checked = false;
+            next28DaysToolStripMenuItem.Checked = false;
+
+            Reminders();
+        }
+
+        private void next21DaysToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            showFor = 21;
+            next7DaysToolStripMenuItem.Checked = false;
+            next14DaysToolStripMenuItem.Checked = false;
+            next21DaysToolStripMenuItem.Checked = true;
+            next28DaysToolStripMenuItem.Checked = false;
+
+            Reminders();
+        }
+
+        private void next28DaysToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            showFor = 28;
+            next7DaysToolStripMenuItem.Checked = false;
+            next14DaysToolStripMenuItem.Checked = false;
+            next21DaysToolStripMenuItem.Checked = false;
+            next28DaysToolStripMenuItem.Checked = true;
+
+            Reminders();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
